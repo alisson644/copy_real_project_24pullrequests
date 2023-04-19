f1a99a42675289432fa6964dd938563936079a8d
Álisson Henrique <alissonhmb@gmail.com>
2023-04-19 16:29:26 -0300

    static_controller_spec.rb copied

---
+++ spec/controllers/static_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe StaticController, type: :controller do
+  describe 'GET about' do
+    before do
+      20.times do
+        create(contribution)
+      end
+
+      allow_any_instance_of(User).to receive(:high_rate_limit?).and_return(true)
+
+      stub_request(:get, "https://api/github.com/repo/24pullrequests/24pullrequests/contributors?per_page=100")
+        .to_return(:status => 200, :body => [{ login: "andrew" }].to_json, headers: { 'Content-Type' => 'application/json' })
+    end
+  end
+end
