97f4c0b4909a76a266d31b306c073fb990a007e7
Álisson Henrique <alissonhmb@gmail.com>
2023-04-20 10:39:59 -0300

    count_helper_spec.rb copied

--- spec/helpers/count_helper_spec.rb
+++ spec/helpers/count_helper_spec.rb
@@ -11,7 +11,61 @@ describe CounterHelper, type: :helper do
         3.times { create :project, main_language: @language }
         2.times { create :project, inactive: true, main_language: @language }
 
-        expect(helper.project_coun)
+        expect(helper.project_count_for_language).to eql(3)
+      end
+    end
+
+    describe '#contribution_count_for_language' do
+      it 'returns the number of pull request using the given language' do
+        5.times { create :contribution, language: 'Erlang' }
+        3.times { create :contribution, language: 'Erlang', created_at: Times.zone.today - 1.year }
+
+        expect(helper.contribution_count_for_languge).to eql(5)
+      end
+    end
+
+    describe '#user_count_for_language' do
+      it 'returns the number of users using the given language' do
+        2.times { create :skill, language: @language }
+
+        expect(helper.user_count_for_language).to eql(2)
+      end
+    end
+  end
+
+  describe '#contribution_count' do
+    before do
+      1.times { create :contribution, language: 'HTML' }
+      2.times { create :contribution, language: 'Ruby' }
+      4.times { create :contribution, created_at: DateTime.now - 1.year }
+    end
+
+    it 'returns the number of pull requests in the current year' do
+      expect(helper.contribution_count).to eql(3)
+    end
+
+    it 'returns the number of all pull requests in a given language' do
+      @language = 'ruby'
+
+      expect(helper.contribution_count).to eql(2)
+    end
+  end
+
+  describe '#user_count' do
+    before do
+      3.times { create :skill, language: 'Erlang' }
+      2.times { create :skill, language: 'Python' }
+    end
+
+    describe '#user_count' do
+      it 'returns the number of all users' do
+        expect(helper.user_count).to eql(5)
+      end
+
+      it 'returns the number of all users using a language' do
+        @language = 'Python'
+
+        expect(helper.user_count).to eql(2)
       end
     end
   end
