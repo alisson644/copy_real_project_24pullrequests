dced93e7626363ddc188647cab77bb012d78eef2
Álisson Henrique <alissonhmb@gmail.com>
2023-05-02 17:37:39 -0300

    spec/requests/login_spec.rb copied

---
+++ spec/requests/login_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe 'Loggin in', type: :request do
+  let(:user) { create :user }
+  subject { page }
+
+  context 'when a guest navigates to a page that requires a user session' do
+    before do
+      mock_is_admin
+      mock_github_auth user
+      visit preferences_path
+    end
+
+    specify { expect(current_path).to eq preferences_path }
+  end
+end
