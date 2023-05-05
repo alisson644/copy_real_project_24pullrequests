42ddd3fe4929da039b82517051dba989be6961e1
Álisson Henrique <alissonhmb@gmail.com>
2023-05-05 11:10:24 -0300

    spec/requests/twitter_spec.rb copied

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -19,7 +19,6 @@ describe 'Static pages', type: :request do
     it { is_expected.to have_link('View All', href: pull_requests_path) }
     it { is_expected.to have_link('Suggest a project', href: new_project_path) }
     it { is_expected.to_not have_css('.featured_projects') }
-
   end
 
   context 'homepage when it has featured projects' do
