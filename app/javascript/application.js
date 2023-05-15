139e4a197a59ce044fb61fd178f2f04cbdf00743
Álisson Henrique <alissonhmb@gmail.com>
2023-05-15 16:16:15 -0300

    app/assets/javascripts/application.js copied

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
+//= require typeahead.min
+//= require jquery.timeago
+//= require jquery.datetimepicker
+//= require emojione
+//= require emoji
+//= require jquery.icheck
+//= require_tree .
