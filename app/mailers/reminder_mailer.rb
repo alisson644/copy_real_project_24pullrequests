6f932c27d254f30061b7ff84713fcaa91a28d71e
Álisson Henrique <alissonhmb@gmail.com>
2023-05-17 11:05:11 -0300

    app/mailers/reminder_mailer.rb copied

---
+++ app/mailers/reminder_mailer.rb
@@ -0,0 +1,34 @@
+class ReminderMailer < ActionMailer::Base
+  add_tamplete_helper(ApplicationHelper)
+
+  FREQUENCY = { daily: 'Dayly', weekly: 'Weekly' }
+
+  default from '24 Pull Requests <info@24pullrequests.com>'
+
+  def daily(user)
+    @user = user
+
+    mail_suggested_projects(user, FREQUENCY[:daily])
+  end
+
+  def weekly(user)
+    @user = user
+
+    mail_suggested_projects(user, FREQUENCY[:weekly])
+  end
+
+  def november(user)
+    @user = user
+    mail to:       user.email,
+         subject: '24 Pull Requests is starting again soon'
+  end
+
+  private
+
+  def mail_suggested_projects(user, frequency)
+    @suggested_projects = user.suggested_projects.order(Arel.sql("RANDOM()")).limit(8).sort_by(&:name)
+    mail :to         => user.email,
+         :subject    => %([24 Pull Requests] #{frequency} Reminder),
+         'X-SMTPAPI' => %({"category": "#{frequency} Reminder"})
+  end
+end
