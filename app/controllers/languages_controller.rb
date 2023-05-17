e7cbca486f833c6ecdb434797773c870c906b189
Álisson Henrique <alissonhmb@gmail.com>
2023-05-17 08:30:17 -0300

    app/controllers/languages_controller.rb copied

---
+++ app/controllers/languages_controller.rb
@@ -0,0 +1,37 @@
+class LanguagesController < ApplicationController
+  before_action :set_language
+
+  def show
+    @projects = Project.active.by_language(@language).limit(20)
+    @users = User.order('contributions_count desc').by_language(@language).limit(200).sample(45)
+    @contributions = Contribution.by_language(@language).year(current_year).latest(5)
+  end
+
+  def projects
+    @projects = Project.active.by_language(@language).page(params[:page])
+  end
+
+  def pull_requests
+    @contributions = Contribution.by_language(@language).year(current_year).page(params[:page])
+
+    render 'contributions/index'
+  end
+
+  def users
+    @users = User.order('contributions_count desc, nickname asc').by_language(@language).page(params[:page])
+
+    render 'users/index'
+  end
+
+  private
+
+  def set_language
+    @language = Project::LANGUAGES.detect { |lan| lan.downcase.eql? language.downcase }
+
+    fail(ActionController::RoutingError, "#{language} is not a valid language") if @language.nil?
+  end
+
+  def language
+    params[:id]
+  end
+end
