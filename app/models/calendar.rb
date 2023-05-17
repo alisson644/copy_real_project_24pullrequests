bc9ad44f0ed68318489039ae20133d2b937f4650
Álisson Henrique <alissonhmb@gmail.com>
2023-05-17 12:22:45 -0300

    app/models/calendar.rb copied

---
+++ app/models/calendar.rb
@@ -0,0 +1,24 @@
+class Calendar
+  include Enumerable
+
+  attr_reader :giftable_dates, :gifts
+
+  def initialize(giftable_dates, gifts)
+    @giftable_dates = giftable_dates
+    @gifts          = gifts
+  end
+
+  def each
+    giftable_dates.each do |date|
+      gift = gifts.find { |g| g.date == date }
+
+      yield(date, gift)
+    end
+  end
+
+  alias_method :each_day, :each
+
+  def start_padding
+    giftable_dates.first.wday - 1
+  end
+end
