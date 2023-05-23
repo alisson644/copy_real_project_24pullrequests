3c63efae1392a0d18117be9f5c640efeb9563267
Álisson Henrique <alissonhmb@gmail.com>
2023-05-23 14:07:42 -0300

    config/initializers/cookies_serializer.rb copied

---
+++ config/initializers/cookies_serializer.rb
@@ -0,0 +1,5 @@
+# Be sure to restart your server when you modify this file
+
+# Specify a serializer for signed and encryped cookie jars.
+# Valid options are :json, :marshal, and :hybrid
+Rails.application.config.action_dispatch.cookies_serializer = :marshal
