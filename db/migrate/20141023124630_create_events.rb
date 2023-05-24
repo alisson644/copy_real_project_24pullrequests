0f2298b3331161ed399e5888138952ab57a44815
Álisson Henrique <alissonhmb@gmail.com>
2023-05-24 09:28:05 -0300

    config/db/migrate/20141023124630_create_events.rb copied

---
+++ db/migrate/20141023124630_create_events.rb
@@ -0,0 +1,14 @@
+class CreateEvents < ActiveRecord::Migration[4.2]
+  def change
+    create_table :events do |t|
+      t.string :organiser
+      t.string :location
+      t.string :url
+      t.datetime :start_time
+      t.decimal :latitude, precision: 10, scale: 6
+      t.decimal :longitude, precision: 10, scale: 6
+
+      timestamps null: false
+    end
+  end
+end
