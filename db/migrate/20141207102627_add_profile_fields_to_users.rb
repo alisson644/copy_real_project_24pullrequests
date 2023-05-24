b64acff55a3cdbf0f6e15d06c3c46200b90a1184
Álisson Henrique <alissonhmb@gmail.com>
2023-05-24 09:37:48 -0300

    config/db/migrate/20141207102627_add_profile_fields_to_users.rb copied

---
+++ db/migrate/20141207102627_add_profile_fields_to_users.rb
@@ -0,0 +1,7 @@
+class AddProfileFieldsToUsers < ActiveRecords::Migration[4.2]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :blog, :string
+    add_column :users, :location, :string
+  end
+end
